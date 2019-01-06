6589beabc342aa92d553ca57167f63ee5a9bd5ca
Varun Mathur <vkmathur97@yahoo.co.uk>
2019-01-06 15:43:28 +0000

    basic curses output

--- src/listscreen.rs
+++ src/listscreen.rs
@@ -1,7 +1,8 @@
 use crate::screen::Screen;
 use crate::data::StockDataDisplay;
 use crate::output::MyWindow;
-use pancurses::Window;
+use crate::output::{align_left, align_centre, align_right};
+
 
 pub struct ListScreen {
     data: Vec<StockDataDisplay>,
@@ -15,6 +16,39 @@ impl ListScreen {
             win: win
         }
     }
+
+    pub fn paint_row(&self, data: &StockDataDisplay, row: i32){
+        let mut col = 0;
+        self.win.win.mvaddstr(row, col, data.ticker.clone());
+        col += 10;
+        self.win.win.mvaddstr(row, col, data.last_price.clone());
+        col += 10;
+        self.win.win.mvaddstr(row, col, data.change.clone());
+        col += 10;
+        self.win.win.mvaddstr(row, col, data.change_p.clone());
+        col += 10;
+        self.win.win.mvaddstr(row, col, data.open.clone());
+        col += 10;
+        self.win.win.mvaddstr(row, col, data.high.clone());
+        col += 10;
+        self.win.win.mvaddstr(row, col, data.low.clone());
+        col += 10;
+        self.win.win.mvaddstr(row, col, data.close.clone());
+    }
+
+    pub fn paint_headers(&self, row: i32){
+        let headers = vec!["SYM","LAST", "CHG", "CHG%", "O", "H", "L", "C"];
+        let mut col = 0;
+        for header in headers.iter(){
+            self.win.win.mvaddstr(row, col, header);
+            col += 10;
+        }
+    }
+
+    pub fn paint_titles(&mut self){
+        self.win.win.mvaddstr(0,0,"STOCKS");
+    }
+        
 }
 
 impl Screen<Vec<StockDataDisplay>> for ListScreen {
@@ -23,7 +57,14 @@ impl Screen<Vec<StockDataDisplay>> for ListScreen {
     }
 
     fn paint(&mut self){
-        self.win.win.mvaddstr(10, 10, "TEST");
+        self.paint_titles();
+        let mut row = 1;
+        self.paint_headers(row);
+        row += 1;
+        for d in self.data.iter(){
+            self.paint_row(d, row);
+            row += 1;
+        }
     }
 
     fn clear(&mut self){
--- src/output.rs
+++ src/output.rs
@@ -13,3 +13,25 @@ impl MyWindow {
 }
 
 unsafe impl Send for MyWindow {}
+
+pub fn align_left(s: String, space: usize) -> String {
+    let trunc: String = s.chars().take(space).collect();
+    let diff: usize = space - trunc.chars().count();
+    let spaces = std::iter::repeat(" ").take(diff).collect::<String>();
+    format!("{}{}", spaces,trunc)
+}
+
+pub fn align_centre(s: String, space: usize) -> String {
+    let trunc: String = s.chars().take(space).collect();
+    let diff = space - trunc.chars().count();
+    let spaces_left = (std::iter::repeat(" ").take(diff / 2).collect::<String>());
+    let spaces_right = (std::iter::repeat(" ").take(diff / 2 + diff % 2).collect::<String>()); 
+    format!("{}{}{}", spaces_left, trunc, spaces_right)
+}
+
+pub fn align_right(s: String, space: usize) -> String {
+    let trunc: String = s.chars().take(space).collect();
+    let diff = space - trunc.chars().count();
+    let spaces = std::iter::repeat(" ").take(diff).collect::<String>();
+    format!("{}{}", trunc, spaces)
+}
